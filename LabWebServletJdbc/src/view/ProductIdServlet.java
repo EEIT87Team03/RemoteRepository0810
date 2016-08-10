79f7399aa71ca52c86521fb1b7597e178ab2ecf3
黃柏翔 <BillyHuang@gmail.com>
2016-08-10 14:27:23 +0800

    //////

--- LabWebServletJdbc/src/view/ProductIdServlet.java
+++ LabWebServletJdbc/src/view/ProductIdServlet.java
@@ -25,6 +25,7 @@ public class ProductIdServlet extends HttpServlet {
 		PrintWriter out = response.getWriter();
 		StringBuilder output = new StringBuilder();
 		///
+		//////
 //接收資料、驗證資料、轉換資料
 		String temp = request.getParameter("id");
 		int id = 0;
